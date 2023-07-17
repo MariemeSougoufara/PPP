bed434c465478eba517e064107d4587ad38e266c
Daniel <107406034+M0onstruck@users.noreply.github.com>
2023-07-17 11:15:41 +0000

    Ligne 5

--- lib/presentation/rate_your_trip_screen/rate_your_trip_screen.dart
+++ lib/presentation/rate_your_trip_screen/rate_your_trip_screen.dart
@@ -14,128 +14,138 @@ class RateYourTripScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: Scaffold(
-            backgroundColor: appTheme.yellow600,
-            resizeToAvoidBottomInset: false,
-            appBar: CustomAppBar(
-                height: getVerticalSize(48),
-                leadingWidth: 28,
-                leading: AppbarImage(
-                    height: getVerticalSize(20),
-                    width: getHorizontalSize(12),
-                    svgPath: ImageConstant.imgArrowleft,
-                    margin: getMargin(left: 16, top: 13, bottom: 15),
-                    onTap: () {
-                      onTapArrowleft(context);
-                    }),
-                centerTitle: true,
-                title: AppbarTitle(text: "Evaluation")),
-            body: Container(
-                height: getVerticalSize(719),
-                width: double.maxFinite,
-                padding: getPadding(left: 16, top: 49, right: 16, bottom: 49),
-                child: Stack(alignment: Alignment.topCenter, children: [
-                  Align(
-                      alignment: Alignment.center,
-                      child: Container(
-                          padding: getPadding(
-                              left: 16, top: 21, right: 16, bottom: 21),
-                          decoration: AppDecoration.outline1.copyWith(
-                              borderRadius: BorderRadiusStyle.roundedBorder8),
-                          child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                Padding(
-                                    padding: getPadding(top: 43),
-                                    child: Text("Abdou Diagne",
-                                        overflow: TextOverflow.ellipsis,
-                                        textAlign: TextAlign.left,
-                                        style: theme.textTheme.titleMedium)),
-                                Text("652 - UKW",
-                                    overflow: TextOverflow.ellipsis,
-                                    textAlign: TextAlign.left,
-                                    style:
-                                        TextThemeHelper.bodyMediumBluegray400),
-                                Padding(
-                                    padding: getPadding(top: 24),
-                                    child: Text(
-                                        "Comment se passe votre voyage ?",
-                                        overflow: TextOverflow.ellipsis,
-                                        textAlign: TextAlign.left,
-                                        style: TextThemeHelper.titleLargeBold
-                                            .copyWith(
-                                                letterSpacing:
-                                                    getHorizontalSize(0.29)))),
-                                Container(
-                                    width: getHorizontalSize(259),
-                                    margin:
-                                        getMargin(left: 25, top: 18, right: 25),
-                                    child: Text(
-                                        "Vos commentaires nous aideront à améliorer l'expérience de conduite",
-                                        maxLines: 2,
-                                        overflow: TextOverflow.ellipsis,
-                                        textAlign: TextAlign.center,
-                                        style: TextThemeHelper
-                                            .bodyLargeBluegray400
-                                            .copyWith(
-                                                letterSpacing:
-                                                    getHorizontalSize(0.41)))),
-                                CustomImageView(
-                                    svgPath: ImageConstant.imgStars,
-                                    height: getVerticalSize(44),
-                                    width: getHorizontalSize(280),
-                                    margin: getMargin(top: 28)),
-                                CustomTextFormField(
-                                    focusNode: FocusNode(),
-                                    autofocus: true,
-                                    controller: commentController,
-                                    margin: getMargin(top: 50),
-                                    contentPadding: getPadding(
-                                        left: 16,
-                                        top: 13,
-                                        right: 16,
-                                        bottom: 13),
-                                    textStyle: TextThemeHelper.bodyLargeGray400,
-                                    hintText: "Commentaires supplémentaires...",
-                                    hintStyle: TextThemeHelper.bodyLargeGray400,
-                                    maxLines: 5,
-                                    filled: true,
-                                    fillColor:
-                                        appTheme.blueGray5001.withOpacity(0.5),
-                                    defaultBorderDecoration:
-                                        TextFormFieldStyleHelper
-                                            .outlineBluegray5002,
-                                    enabledBorderDecoration:
-                                        TextFormFieldStyleHelper
-                                            .outlineBluegray5002,
-                                    focusedBorderDecoration:
-                                        TextFormFieldStyleHelper
-                                            .outlineBluegray5002,
-                                    disabledBorderDecoration:
-                                        TextFormFieldStyleHelper
-                                            .outlineBluegray5002),
-                                CustomElevatedButton(
-                                    text: "Soumettre un avis",
-                                    margin: getMargin(top: 29),
-                                    buttonStyle: ButtonThemeHelper
-                                        .fillIndigoA400
-                                        .copyWith(
-                                            fixedSize:
-                                                MaterialStateProperty.all<Size>(
-                                                    Size(double.maxFinite,
-                                                        getVerticalSize(45)))),
-                                    buttonTextStyle:
-                                        TextThemeHelper.titleMediumPrimary_1)
-                              ]))),
-                  CustomImageView(
-                      imagePath: ImageConstant.imgOval295x95,
-                      height: getSize(95),
-                      width: getSize(95),
-                      radius: BorderRadius.circular(getHorizontalSize(47)),
-                      alignment: Alignment.topCenter)
-                ]))));
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, AppRoutes.tipsScreen);
+      },
+      child: SafeArea(
+          child: Scaffold(
+              backgroundColor: appTheme.yellow600,
+              resizeToAvoidBottomInset: false,
+              appBar: CustomAppBar(
+                  height: getVerticalSize(48),
+                  leadingWidth: 28,
+                  leading: AppbarImage(
+                      height: getVerticalSize(20),
+                      width: getHorizontalSize(12),
+                      svgPath: ImageConstant.imgArrowleft,
+                      margin: getMargin(left: 16, top: 13, bottom: 15),
+                      onTap: () {
+                        onTapArrowleft(context);
+                      }),
+                  centerTitle: true,
+                  title: AppbarTitle(text: "Evaluation")),
+              body: Container(
+                  height: getVerticalSize(719),
+                  width: double.maxFinite,
+                  padding: getPadding(left: 16, top: 49, right: 16, bottom: 49),
+                  child: Stack(alignment: Alignment.topCenter, children: [
+                    Align(
+                        alignment: Alignment.center,
+                        child: Container(
+                            padding: getPadding(
+                                left: 16, top: 21, right: 16, bottom: 21),
+                            decoration: AppDecoration.outline1.copyWith(
+                                borderRadius: BorderRadiusStyle.roundedBorder8),
+                            child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Padding(
+                                      padding: getPadding(top: 43),
+                                      child: Text("Abdou Diagne",
+                                          overflow: TextOverflow.ellipsis,
+                                          textAlign: TextAlign.left,
+                                          style: theme.textTheme.titleMedium)),
+                                  Text("652 - UKW",
+                                      overflow: TextOverflow.ellipsis,
+                                      textAlign: TextAlign.left,
+                                      style: TextThemeHelper
+                                          .bodyMediumBluegray400),
+                                  Padding(
+                                      padding: getPadding(top: 24),
+                                      child: Text(
+                                          "Comment se passe votre voyage ?",
+                                          overflow: TextOverflow.ellipsis,
+                                          textAlign: TextAlign.left,
+                                          style: TextThemeHelper.titleLargeBold
+                                              .copyWith(
+                                                  letterSpacing:
+                                                      getHorizontalSize(
+                                                          0.29)))),
+                                  Container(
+                                      width: getHorizontalSize(259),
+                                      margin: getMargin(
+                                          left: 25, top: 18, right: 25),
+                                      child: Text(
+                                          "Vos commentaires nous aideront à améliorer l'expérience de conduite",
+                                          maxLines: 2,
+                                          overflow: TextOverflow.ellipsis,
+                                          textAlign: TextAlign.center,
+                                          style: TextThemeHelper
+                                              .bodyLargeBluegray400
+                                              .copyWith(
+                                                  letterSpacing:
+                                                      getHorizontalSize(
+                                                          0.41)))),
+                                  CustomImageView(
+                                      svgPath: ImageConstant.imgStars,
+                                      height: getVerticalSize(44),
+                                      width: getHorizontalSize(280),
+                                      margin: getMargin(top: 28)),
+                                  CustomTextFormField(
+                                      focusNode: FocusNode(),
+                                      autofocus: true,
+                                      controller: commentController,
+                                      margin: getMargin(top: 50),
+                                      contentPadding: getPadding(
+                                          left: 16,
+                                          top: 13,
+                                          right: 16,
+                                          bottom: 13),
+                                      textStyle:
+                                          TextThemeHelper.bodyLargeGray400,
+                                      hintText:
+                                          "Commentaires supplémentaires...",
+                                      hintStyle:
+                                          TextThemeHelper.bodyLargeGray400,
+                                      maxLines: 5,
+                                      filled: true,
+                                      fillColor: appTheme.blueGray5001
+                                          .withOpacity(0.5),
+                                      defaultBorderDecoration:
+                                          TextFormFieldStyleHelper
+                                              .outlineBluegray5002,
+                                      enabledBorderDecoration:
+                                          TextFormFieldStyleHelper
+                                              .outlineBluegray5002,
+                                      focusedBorderDecoration:
+                                          TextFormFieldStyleHelper
+                                              .outlineBluegray5002,
+                                      disabledBorderDecoration:
+                                          TextFormFieldStyleHelper
+                                              .outlineBluegray5002),
+                                  CustomElevatedButton(
+                                      text: "Soumettre un avis",
+                                      margin: getMargin(top: 29),
+                                      buttonStyle: ButtonThemeHelper
+                                          .fillIndigoA400
+                                          .copyWith(
+                                              fixedSize: MaterialStateProperty
+                                                  .all<Size>(Size(
+                                                      double.maxFinite,
+                                                      getVerticalSize(45)))),
+                                      buttonTextStyle:
+                                          TextThemeHelper.titleMediumPrimary_1)
+                                ]))),
+                    CustomImageView(
+                        imagePath: ImageConstant.imgOval295x95,
+                        height: getSize(95),
+                        width: getSize(95),
+                        radius: BorderRadius.circular(getHorizontalSize(47)),
+                        alignment: Alignment.topCenter)
+                  ])))),
+    );
   }
 
   /// Navigates back to the previous screen.
